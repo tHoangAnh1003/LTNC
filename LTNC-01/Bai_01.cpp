97546f856ec9a53bb3f14b0309a461d6d4605fa4
TranHoangAnh <tanh23274@gmail.com>
2023-07-20 19:17:15 +0700

    Bai_01

---
+++ LTNC-01/Bai_01.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 1e9
+#define MIN -1e9
+#define MOD int(1e9 + 7)
+typedef long long ll;
+
+signed main() {
+    cout << "Hello, World!";
+    return 0;
+}
