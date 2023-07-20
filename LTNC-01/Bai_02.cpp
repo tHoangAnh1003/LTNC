747c3cd51d29fdf4b4573255ee7ed2f1478c408a
TranHoangAnh <tanh23274@gmail.com>
2023-07-20 19:19:54 +0700

    Bai_02

---
+++ LTNC-01/Bai_02.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 1e9
+#define MIN -1e9
+#define MOD int(1e9 + 7)
+typedef long long ll;
+
+signed main() {
+    int a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    int sum = a + b +c;
+    
+    cout << sum;   
+    return 0;
+}
